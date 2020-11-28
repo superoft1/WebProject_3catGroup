0e28b284f2296f9f4b514c268c708ff8addb7881
superoft1 <superoft1@users.noreply.github.com>
2020-11-28 22:06:24 +0700

    update interface

--- Final Project/models/detailModel.js
+++ Final Project/models/detailModel.js
@@ -1,7 +1,7 @@
 exports.list = () => {
     return [
         {
-            id: 1,
+            id: 0,
             title: 'The Two Towers : The Lord of the Rings, Part 2',
             image: 'images/b1.jpg',
             basePrice: 157000,
@@ -19,7 +19,7 @@ exports.list = () => {
             description: "Building on the story begun in The Hobbit and The Fellowship of the Ring, this is the second part of Tolkien's epic masterpiece, The Lord of the Rings, featuring an exclusive cover image from the film, the definitive text, and a detailed map of Middle-earth. Frodo and the Companions of the Ring have been beset by danger during their quest to prevent the Ruling Ring from falling into the hands of the Dark Lord by destroying it in the Cracks of Doom. They have lost the wizard, Gandalf, in the battle with an evil spirit in the Mines of Moria; and at the Falls of Rauros, Boromir, seduced by the power of the Ring, tried to seize it by force. While Frodo and Sam made their escape the rest of the company were attacked by Orcs. Now they continue their journey alone down the great River Anduin - alone, that is, save for the mysterious creeping figure that follows wherever they go. To celebrate the release of the first of Peter Jackson's two-part film adaptation of The Hobbit, THE HOBBIT: AN UNEXPECTED JOURNEY, this second part of The Lord of the Rings is available for a limited time with an exclusive cover image from Peter Jackson's award-winning trilogy."
         },
         {
-            id: 2,
+            id: 1,
             title: 'Totto-Chan Bên Cửa Sổ (Tái Bản 2019)',
             image: 'images/b2.jpg',
             basePrice: 98000,
@@ -37,7 +37,7 @@ exports.list = () => {
             description: "Vừa vào lớp một được vài ngày, Totto-chan đã bị đuổi học!!! Không còn cách nào khác, mẹ đành đưa Totto-chan đến một ngôi trường mới, trường Tomoe. Đấy là một ngôi trường kỳ lạ, lớp học thì ở trong toa xe điện cũ, học sinh thì được thoả thích thay đổi chỗ ngồi mỗi ngày, muốn học môn nào trước cũng được, chẳng những thế, khi đã học hết bài, các bạn còn được cô giáo cho đi dạo. Đặc biệt hơn ở đó còn có một thầy hiệu trưởng có thể chăm chú lắng nghe Totto-chan kể chuyện suốt bốn tiếng đồng hồ! Chính nhờ ngôi trường đó, một Totto-chan hiếu động, cá biệt đã thu nhận được những điều vô cùng quý giá để lớn lên thành một con người hoàn thiện, mạnh mẽ. Totto-chan bên cửa sổ là cuốn sách gối đầu giường của nhiều thế hệ trẻ em trên toàn thế giới suốt ba mươi năm nay. Sau khi xuất bản lần đầu vào năm 1981, cuốn sách đã gây được tiếng vang lớn không chỉ ở Nhật Bản mà còn trên toàn thế giới. Tính đến năm 2001, tổng số bản sách bán ra ở Nhật đã lên đến 9,3 triệu bản, trở thành một trong những cuốn sách bán chạy nhất trong lịch sử ngành xuất bản nước này. Cuốn sách đã được dịch ra 33 thứ tiếng khác nhau, như Anh, Pháp, Đức, Hàn Quốc, Trung Quốc…Khi bản tiếng Anh của Totto-chan được xuất bản tại Mỹ, tờ New York Times đã đăng liền hai bài giới thiệu trọn trang, một “vinh dự” hầu như không tác phẩm nào có được."
         },
         {
-            id: 3,
+            id: 2,
             title: 'Colorful',
             image: 'images/b3.jpg',
             basePrice: 80000,
@@ -55,7 +55,7 @@ exports.list = () => {
             description: "Có một người phạm tội nặng, chết đi không được luân hồi. Nhưng trong lúc linh hồn người này đang mất trí nhớ và trôi nổi vô định về một nơi tối tăm xứng đáng với cậu ta, thì một thiên sứ cánh trắng xuất hiện, giơ tay chặn lại, thông báo rằng cậu vừa trúng phiên xổ số may mắn của thiên đình, nhận được cơ hội tu hành kiêm tái thử thách. Theo đó, hồn cậu sẽ quay về trần gian, nhập vào xác một người vừa tự sát, ở trọ nhà người ta, nếm trải nhân tình thế thái và xử lý đống hỗn độn mà họ để lại. Mức độ hỗn độn tương đương với mức độ tội lỗi cậu phạm phải trong kiếp trước. Nếu việc tu hành diễn ra thuận lợi, ký ức kiếp trước sẽ dần dần quay về. Giây phút nhớ được tội lỗi mình đã gây ra cũng là lúc kết thúc quá trình ở trọ. Linh hồn cậu sẽ rời khỏi thân xác đi mượn và quay lên thiên giới, thuận lợi đặt chân vào vòng luân hồi, đầu thai sang kiếp khác. Colorful men theo những bất an bình thường mà ai cũng có thể gặp phải trong cuộc sống, nhưng một số người lại thấy chúng quá dị thường, quá kinh khủng, dẫn đến khổ tâm dằn vặt và rồi không thể vượt được qua. Để giải phóng những bình thường ấy, Colorful trình chiếu một giải pháp đặc biệt khác thường. Cuộc đời có muôn vàn nghiệm đúng, người ta tùy ý sống theo cách mình thích, nhưng một khi đã rút lui khỏi nó, thì chẳng còn nghiệm nào cho ta lựa chọn cả."
         },
         {
-            id: 4,
+            id: 3,
             title: 'Sherlock Holmes Toàn Tập - Tập 1',
             image: "images/b4.jpg",
             basePrice: 122000,
@@ -73,7 +73,7 @@ exports.list = () => {
             description: "Từ khi xuất hiện lần đầu tiên vào năm 1887, thám tử Sherlock Holmes đã trở thành một trong những nhân vật hư cấu được yêu thích nhất. Là bậc thầy về khoa học suy luận, có thể làm sáng tỏ ngay cả những vụ án khó và phức tạp nhất bằng cách phát hiện ra những manh mối mà với người khác là quá mơ hồ và không thể nhận biết, thám tử Sherlock Holmes là nhân vật chính trong bộ tác phẩm gồm sáu mươi câu chuyện được viết bởi Sir Arthur Conan Doyle từ năm 1887 đến năm 1927. Với sự giúp đỡ của người bạn tận tụy và trung thành là bác sĩ Watson, thám tử Sherlock Holmes đã khiến vô số những kẻ lừa đảo, kẻ cướp và kẻ sát nhân bị đưa ra truy tố trước pháp luật. Nhiều câu chuyện về Sherlock Holmes đã được xếp vào hàng những tác phẩm trinh thám hư cấu hay nhất mọi thời đại, trong số đó có 'Chiếc nhẫn tình cờ', 'Vụ bê bối ở Bohemia', 'Dải băng lốm đốm' và 'Con chó săn của dòng họ Baskerville'. Bộ Sherlock Holmes toàn tập này sẽ giới thiệu với bạn đọc trọn bộ tác phẩm gồm 56 truyện ngắn và 4 tiểu thuyết, được sắp xếp theo trình tự thời gian xuất bản. Tập một: Chiếc nhẫn tình cờ (tiểu thuyết), Dấu bộ tứ (tiểu thuyết), Những cuộc phiêu lưu của Sherlock Holmes (12 truyện ngắn). Tập hai: Hồi ức của Sherlock Holmes (11 truyện ngắn), Sherlock Holmes trở về (13 truyện ngắn), Con chó săn của dòng họ Baskerville (tiểu thuyết). Tập ba: Thung lũng khủng khiếp (tiểu thuyết), Cung đàn sau cuối (18 truyện ngắn), Tàng thư của Sherlock Holmes (12 truyện ngắn). Hy vọng bộ sách này sẽ mang đến cho độc giả những giờ phút thư giãn thú vị và trở thành một phần không thể thiếu trong tủ sách của mỗi gia đình."
         },
         {
-            id: 5,
+            id: 4,
             title: 'Tiếng Việt 1 - Tập 1 (Bộ Sách Cánh Diều)',
             image: 'images/b5.jpg',
             basePrice: 34000,
